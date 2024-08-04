701d270c68ca06ee0dd44d0eea6a566f125dc607
Elijah Mejilla <111504836+jgmejilla@users.noreply.github.com>
2024-08-04 18:19:53 +0800

    fix: hopefully

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ class Flashcard {
 }
 
 async function makeFlashcards() {
-    const response = await fetch('./../preprocess/n5_processed.csv')    
+    const response = await fetch('./preprocess/n5_processed.csv')    
     const text = await response.text()
     const lines = text.split('\n').slice(1) 
 
